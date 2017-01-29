7ff7d27d711dc93ab98883273ef6606e7c0167fc
Larissa Yasin <larissa.yasin@gmail.com>
2017-01-29 13:23:35 -0200

    Primeiro Commit

---
+++ PersonagensMarvel/ListaTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersonagensMarvel
+//
+//  Created by Larissa Gonçalves on 27/01/17.
+//  Copyright © 2017 LarissaYasin. All rights reserved.
+//
+
+import UIKit
+
+class ListaTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
